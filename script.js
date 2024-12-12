db86156fd687f99a1b1c14b3b69cbc5de5667e92
vklepetko <109442883+vklepetko@users.noreply.github.com>
2024-12-12 10:07:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function refresh(){
   var b = getSemiMinor(a,e);
   var w = window.innerWidth;
   var h = window.innerHeight;
-  var c = ((Number(document.getElementById('checkbox_center_focus').checked) ==1) ?  w/3*2 - e*a : w/3*2);
+  var c = ((Number(document.getElementById('checkbox_center_focus').checked) ==1) ?  w*0.618 - e*a : w*0.618);
   
   document.getElementById('a_descr').innerHTML = a;
   document.getElementById('e_descr').innerHTML = e;
