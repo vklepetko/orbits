3b62dbec05b74b9927098a6ef5d55d69ec9b0aeb
vklepetko <109442883+vklepetko@users.noreply.github.com>
2024-12-13 19:35:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var color_array = ["#dc4048","#7acdf1","#feb913","#f6821f","#003f6b" ];
 var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
 svg.setAttribute("width","100%");
 svg.setAttribute("height","100%");
-svg.setAttribute("preserveAspectRatio","none");
+//svg.setAttribute("preserveAspectRatio","none");
 zoom(document.getElementById('slider_fov').value);
 
 
