be5e9962f2b732e8963904ac6c9d610506bb5e04
vklepetko <109442883+vklepetko@users.noreply.github.com>
2024-12-15 18:33:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
 svg.setAttribute("width","100%");
 svg.setAttribute("height","100%");
 //svg.setAttribute("preserveAspectRatio","none");
-zoom(document.getElementById('slider_fov').value);
+zoom(150);
 
 var anim = document.createElementNS("http://www.w3.org/2000/svg","animate");
 anim.setAttribute('attributeName','viewBox');
