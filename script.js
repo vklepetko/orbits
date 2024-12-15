b023ce57dc2cecb506a22ea8620042e8ced2550a
vklepetko <109442883+vklepetko@users.noreply.github.com>
2024-12-15 19:19:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ svg.setAttribute('viewBox',getViewBoxString(150));
 
 var anim = document.createElementNS("http://www.w3.org/2000/svg","animate");
 anim.setAttribute('attributeName','viewBox');
-anim.setAttribute('to',getViewBoxString(5));
+anim.setAttribute('to',getViewBoxString(15));
 anim.setAttribute('dur','2s');
 anim.setAttribute('fill','freeze');
-anim.setAttribute('onend','zoom(5)');
+anim.setAttribute('onend','zoom(15)');
 svg.appendChild(anim);
 
 
