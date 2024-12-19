d69952e5568dcc9dfbc288ace8e68895a9f9bfdd
vklepetko <109442883+vklepetko@users.noreply.github.com>
2024-12-19 13:13:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ major.setAttribute("x2",cx+rx);
 major.setAttribute("y2",cy);
 major.setAttribute("stroke-width","0.025");
 major.setAttribute("stroke","grey");
-major.setAttribute("stroke-dasharray","0.05");
+major.setAttribute("stroke-dasharray","0.1");
 
   
 
@@ -147,14 +147,14 @@ minor.setAttribute("x2",cx);
 minor.setAttribute("y2",cy+ry);
 minor.setAttribute("stroke-width","0.025");
 minor.setAttribute("stroke","grey");
-minor.setAttribute("stroke-dasharray","0.05");
+minor.setAttribute("stroke-dasharray","0.1");
 
 var focus2 = document.createElementNS("http://www.w3.org/2000/svg","circle");
 focus2.setAttribute("cx",cx-epsilon);
 focus2.setAttribute("cy",cy);
 focus2.setAttribute("stroke","grey");
 focus2.setAttribute("fill","none");
-focus2.setAttribute("stroke-width","0.005");
+focus2.setAttribute("stroke-width","0.025");
 focus2.setAttribute("r","0.05");
 
 svg.appendChild(major);
