6ac8bc8518f165320361c0fcd52e042fb7f32fa2
vklepetko <109442883+vklepetko@users.noreply.github.com>
2024-12-20 20:16:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ function init(){
 function exportSVG(e){
   let svg = document.querySelector('svg').outerHTML;
   let blob = new Blob([svg], {type : 'image/svg+xml'});
-  e.target.href = URL.createObjectURL(blob);
+  e.location.href = URL.createObjectURL(blob);
 	
 };
 
